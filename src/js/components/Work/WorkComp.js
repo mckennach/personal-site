fe74fa55e2efee59b08469998b257cd11548084a
Chris Mckenna <mckennach@gmail.com>
2018-01-30 21:26:48 -0500

    merge

--- src/js/components/Work/WorkComp.js
+++ src/js/components/Work/WorkComp.js
@@ -2,14 +2,13 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 import WorkIcon from './WorkIcon';
 import Paragraph from '../Paragraph';
-<<<<<<< HEAD
+
 import {
 
   NavLink,
 
 } from 'react-router-dom'
-=======
->>>>>>> develop
+
 
 
 export default class WorkComp extends React.Component{
@@ -19,7 +18,7 @@ export default class WorkComp extends React.Component{
       this.projects = [
         {
           title: 'Amatis Controls',
-<<<<<<< HEAD
+
           icon: '../../img/amatis.png',
           alt: "work-card-1",
           path: '/amatis',
@@ -28,17 +27,12 @@ export default class WorkComp extends React.Component{
           style: {
             backgroundColor: 'black'
           }
-=======
-          secondTitle: 'Shortshort',
-          icon: '../../img/android-icon.png',
-          id: 1,
-          desc: 'This App interfaces with the Amatis Controls Lighting system to give the user extremely quick and responsive control over their lighting.'
->>>>>>> develop
+
         },
         {
           title: 'AML Entertainment',
           icon: '../../img/aml.png',
-<<<<<<< HEAD
+
           alt: "work-card-2",
           path: '/aml',
           id: 2,
@@ -57,7 +51,7 @@ export default class WorkComp extends React.Component{
           id: 3,
           type: 'Design / Branding / Front End Developemnt',
           style: {
-            
+
           }
 
 
@@ -83,43 +77,16 @@ export default class WorkComp extends React.Component{
         </div>
 
 
-=======
-          id: 2,
-          desc: 'this will be a desc'
-        },
-        {
-          title: 'Charles P. Braman Real Estate Appraisals',
-          icon: '../../img/straw.png',
-          id: 3,
-          desc: 'this will be a desc'
-        },
-      ]
-  }
 
-
-    render() {
-
-      const component = this.projects.map((project) =>
-      <Paragraph
-        key={project.id}
-        id={project.id}
-        className="paragraph"
-        title={project.title}
-        secondTitle={project.title}
-        text={project.desc}
-      />
->>>>>>> develop
 
 
       )
 
 
       return(
-<<<<<<< HEAD
+
           <div className="work-card-container">
-=======
-          <div>
->>>>>>> develop
+
             {component}
           </div>
        );
